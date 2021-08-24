6568430aa7cb43c619584b991d21ac216e368e3e
John Binken <jcjbinken@gmail.com>
2021-08-24 19:59:56 +0300

    1.4 done

--- osa1/1.4/src/App.js
+++ osa1/1.4/src/App.js
@@ -19,16 +19,23 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      {props.p1}
+      {props.p1.map(elem => (
+        <p>{elem.name} {elem.exercises} </p>
+      ))}
+      
     </div>
   );
 };
 
 const Total = (props) => {
- 
+  const total = props.p1.reduce(
+    (a, b) => a + b.exercises,
+    0
+  );
   return (
     <div>
-      <p> Total number of exercises {props.nE + props.nE2 + props.nE3}</p>
+      <p>Total Exercises: {total}</p>
+      
     </div>
   );
 };
@@ -36,12 +43,14 @@ const Total = (props) => {
   
     const App = () => {
       const course = 'Half Stack application development'
+    
       const parts = [
         {
           name: 'Fundamentals of React',
           exercises: 10
         },
         {
+
           name: 'Using props to pass data',
           exercises: 7
         },
@@ -53,9 +62,11 @@ const Total = (props) => {
 
   return (
     <div>
+      
+    
       <Header course={course} />
       <Content p1={parts} />
-      <Total/>
+      <Total p1={parts}/>
     </div>
   );
 };
